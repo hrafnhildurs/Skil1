2324747e1b18614446942e7305a2a68cebd97b3d
einirjr <einir@einir.com>
2015-11-30 12:12:58 +0000

    Buinn til interface klasi

---
+++ Skil1/interface.h
@@ -0,0 +1,25 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include "person.h"
+
+
+class Interface
+{
+public:
+    void selection();
+private:
+    void DocString (const char doc[], std::vector<std::string> stringVec);
+    void alphabeticSortAsc();
+    void alphabeticSortDes();
+    void search(const char doc[]);
+    void sortMenu();
+};
+
+#endif // INTERFACE_H
